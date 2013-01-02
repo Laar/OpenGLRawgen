1f3701e9ea5372e4112746352f5ae7f0974d832e
Lars Corbijn <aspergesoepje@gmail.com>
2013-01-02 13:37:57 +0100

    Correct testrun to recursive diff

--- testrun.sh
+++ testrun.sh
@@ -2,13 +2,19 @@
 REFERENCEDIR="referenceoutput"
 TESTDIR="testoutput"
 
+rm -r "$TESTDIR"
+mkdir "$TESTDIR"
 echo "---------------------------"
 echo "-- Generating new output --"
 echo "---------------------------"
 time ./OpenGLRawgen --no-vendorf=input/novendor -o "$TESTDIR" -c -d input +RTS -sstderr &&
-echo  "---------------"
-echo  "-- Comparing --"
-echo  "---------------"
 
-exec diff "$TESTDIR" "$REFERENCEDIR"
+if [[ $? == 0 ]] ;
+then
+    echo  "---------------"
+    echo  "-- Comparing --"
+    echo  "---------------"
+
+    exec diff -r "$TESTDIR" "$REFERENCEDIR"
+fi
 
