08445ce02604e59fb9e63af69f480370fab6f41a
Lars Corbijn <aspergesoepje@gmail.com>
2013-03-26 17:47:50 +0100

    Adds a dependency install script.

---
+++ depsinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+	echo "Needs an cabal command as first argument"
+	exit 1
+fi
+CABAL="$1"
+git submodule update --init --recursive
+$CABAL install opengl-api/ CodeGenerating/ OpenGLRawgenBase/OpenGLRawgenBase/
+$CABAL install --only-dependencies
