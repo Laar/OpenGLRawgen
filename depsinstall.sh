7d4a565b0162aa5079fc516a2f4ca60f15cd512c
Lars Corbijn <aspergesoepje@gmail.com>
2014-11-04 10:21:00 +0100

    More info on cabal.

--- depsinstall.sh
+++ depsinstall.sh
@@ -7,6 +7,7 @@ fi
 CABAL="$1"
 ghc-pkg list
 git submodule update --init --recursive
+$CABAL --version
 $CABAL install CodeGenerating/ OpenGLRawgenBase/OpenGLRawgenBase/ OpenGLRawgenBase/OpenGLRawInterface/ opengl-xmlspec/ --dry-run -v
 $CABAL install CodeGenerating/ OpenGLRawgenBase/OpenGLRawgenBase/ OpenGLRawgenBase/OpenGLRawInterface/ opengl-xmlspec/
 $CABAL install --only-dependencies
